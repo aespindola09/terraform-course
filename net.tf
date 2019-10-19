46d676dd193ac5b8f9bcfcf37a0a347dc1905a4a
Alexis Espindola <aespindola09@gmail.com>
2019-10-18 22:23:33 -0300

    install nginx

--- net.tf
+++ net.tf
@@ -2,3 +2,31 @@ resource "google_compute_network" "vpc_network" {
   description = "red de prueba"
   name = "terraform-network"
 }
+
+resource "google_compute_firewall" "terraform-http" {
+  name    = "terraform-http-input"
+  network = "${google_compute_network.vpc_network.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80",]
+
+  }
+target_tags = ["http-server"]  
+}
+
+resource "google_compute_firewall" "terraform-ssh" {
+  name    = "terraform-ssh-input"
+  network = "${google_compute_network.vpc_network.name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+
+  }
+target_tags = ["ssh"]  
+}
--- vms.tf
+++ vms.tf
@@ -1,13 +1,21 @@
 resource "google_compute_instance" "vm_instance" {
     name    = "terraform-instance"
     machine_type = "f1-micro"
-    tags    = ["web", "dev"]    
+    tags    = ["web", "dev", "http-server", "ssh"]
+
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install nginx -y"
 
     boot_disk {
         initialize_params {
             image = "debian-cloud/debian-10"
         }
     }
+
+        scheduling {
+            automatic_restart   = false
+            on_host_maintenance = "TERMINATE"
+            preemptible         = true
+        }
 
     network_interface {
         network = google_compute_network.vpc_network.self_link
